4e7d36262ba63af1742e28f1e307893f0962b4ec
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-11-15 13:53:24 +0700

    same-origin policy

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
 
   server.route(routes);
